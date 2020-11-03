68c14288dc72a4e8f95f3576e333d0d68736b8d7
Kam Mistry <mistrykam@gmail.com>
2020-11-03 13:02:06 -0500

    Add new book page.

---
+++ App.Client.WebUI/Pages/Books/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using App.Domain.Application.Features.Books.Commands;
+using App.Domain.Application.Features.Books.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+
+namespace App.Client.WebUI.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IMediator _mediatR;
+
+        public CreateModel(IMediator mediatR)
+        {
+            _mediatR = mediatR;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public CreateBookCommand Book { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _mediatR.Send(Book);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
