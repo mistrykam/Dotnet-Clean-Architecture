194da3983f9e035d86eb9aee2a7e988f1634fbc6
Kam Mistry <mistrykam@gmail.com>
2020-12-02 10:37:56 -0500

    Added base entity and AuditableEntity class.

---
+++ App.Domain.Entities/Framework/AuditableEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace App.Domain.Entities.Framework
+{
+    public abstract class IAuditableEntity
+    {
+        // When the entity was Created 
+        public string CreatedBy { get; private set; }
+        public DateTime CreatedDateTime { get; private set; }
+
+        // When the entity was Last Updated
+        public string LastModifiedBy { get; private set; }
+        public DateTime LastModifiedDateTime { get; private set; }
+
+        /// <summary>
+        /// Record when and who created the entity
+        /// </summary>
+        /// <param name="user"></param>
+        public void Created(string user)
+        {
+            CreatedBy = user;
+            CreatedDateTime = DateTime.UtcNow;
+
+            LastModifiedBy = user;
+            LastModifiedDateTime = CreatedDateTime;
+        }
+
+        /// <summary>
+        /// Record when and who modified the entity
+        /// </summary>
+        /// <param name="user"></param>
+        public void Modified(string user)
+        {
+            LastModifiedBy = user;
+            LastModifiedDateTime = DateTime.UtcNow;
+        }
+    }
+}
---
+++ App.Domain.Entities/Framework/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Domain.Entities.Framework
+{
+    // Defines the base entity 
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
